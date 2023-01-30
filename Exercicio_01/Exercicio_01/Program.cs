e75088c6caaecbca4ca5f34a0022c5fa245e6c8c
GuilhermeVideira <123119430+GuilhermeVideira@users.noreply.github.com>
2023-01-30 20:18:48 -0300

    Add files via upload

---
+++ Exercicio_01/Exercicio_01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_01
+{
+    internal class Program
+    {
+        public struct Data
+        {
+           public int dia; 
+           public int mes; 
+           public int ano; 
+        }
+
+        static void Main(string[] args)
+        {
+            Data data;
+            Boolean bissexto;
+
+            Console.Write("Digite o dia: ");
+            data.dia = int.Parse(Console.ReadLine());
+            Console.Write("Digite o mês: ");
+            data.mes = int.Parse(Console.ReadLine());
+            Console.Write("Digite o ano com 4 digitos: ");
+            data.ano = int.Parse(Console.ReadLine());
+
+            if ((data.ano % 4 == 0 && data.ano % 100 != 0) || data.ano % 400 == 0)
+                bissexto = true;
+            else
+                bissexto = false;
+
+            if ((data.dia < 1 || (data.dia > 31 ||
+            (data.dia > 28 && data.mes == 2 && !bissexto) ||
+            (data.dia > 29 && data.mes == 2 && bissexto) ||
+            (data.dia > 30 && (data.mes == 4 || data.mes == 6 || data.mes == 9 || data.mes == 11)))) ||
+            (data.mes < 1 || data.mes > 12))
+                Console.WriteLine("Data inválida!!!");
+            else
+                Console.WriteLine("Data válida!!!");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Exercicio_02/Exercicio_02/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_02
+{
+    internal class Program
+    {
+
+        public struct cadaluno
+        { 
+        public string nome;
+        public float nota1;
+        public float nota2;
+        public float media;
+        public string situacao;
+        }
+
+       public static cadaluno[] aluno = new cadaluno[15];
+
+        static void Main(string[] args)
+        {
+            int q;
+            char sair = 'b';
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            while (sair != 'S' && sair != 's')
+            {
+                Console.Clear();
+            retorno:
+                Console.WriteLine(" Cadastro de Aluno");
+                Console.WriteLine(" digite a quantidade de alunos a serem cadastrado de 1 ate 15");
+                q = int.Parse(Console.ReadLine());
+                if (q < 1 || q > 15)
+                {
+                    Console.WriteLine(" o intervalo esta invalido ");
+                    goto retorno;
+                }
+                entrada(q);
+                Console.WriteLine(" Resultado do cadastro ");
+                relatorio(q);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(" Dejesa sair S/N");
+                sair = char.Parse(Console.ReadLine());
+            }
+            Console.ReadKey();
+        }
+
+        //Metódos
+        static void entrada(int quant)
+        {
+            int x, mos = 0;
+            string nome;
+            float nota,media;
+            for (x = 0; x < quant; x++)
+            {
+                mos = x + 1;
+                Console.WriteLine(mos + " º Nome do aluno =");
+                nome = Console.ReadLine();
+                aluno[x].nome = nome;
+                retornon1:
+                Console.WriteLine("1 º nota =");
+                nota = float.Parse(Console.ReadLine());
+                if (nota < 0 || nota > 10)
+                {
+                    Console.WriteLine(" A primeira nota esta invalida 0 até 10 ");
+                    goto retornon1;
+                }
+                aluno[x].nota1 = nota;
+                retornon2:
+                Console.WriteLine("2 º nota =");
+                nota = float.Parse(Console.ReadLine());
+                if (nota < 0 || nota > 10)
+                {
+                    Console.WriteLine(" A segunda nota esta invalida 0 até 10 ");
+                    goto retornon2;
+                }
+                aluno[x].nota2 = nota;
+                media = (aluno[x].nota1 + aluno[x].nota2) / 2;
+                aluno[x].media = media;
+                if (media >= 7)
+                    aluno[x].situacao = "Aprovado";
+                else
+                    aluno[x].situacao = "Reprovado";
+            }
+        }
+
+        static void relatorio(int quant)
+        {
+            int x;
+            Console.Clear();
+            for (x = 0; x < quant; x++)
+            {
+                if (aluno[x].situacao == "Aprovado")
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                else
+                    Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(aluno[x].nome + " \t " + aluno[x].nota1 + " \t " + aluno[x].nota2 + " \t " + aluno[x].media + "\t " + aluno[x].situacao);
+            }
+
+
+        }
+    }
+}
